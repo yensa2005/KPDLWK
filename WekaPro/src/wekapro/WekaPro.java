b2f6a68a2f698524d459588ef16a1d50e90856af
Yen Sa <ttysa98tcv@gmail.com>
2021-03-04 13:34:57 +0700

    test

--- WekaPro/src/wekapro/WekaPro.java
+++ WekaPro/src/wekapro/WekaPro.java
@@ -27,7 +27,7 @@ public class WekaPro {
 //     
 //        model.mineAssociationRules();
 //        System.out.println(model);
-        
+
 //    MyAprioriModel model = new MyAprioriModel("C:\\Program Files\\Weka-3-8-5\\data\\weather.numeric.arff",
 //                                "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
 //                                "-R 2-3");
@@ -35,12 +35,11 @@ public class WekaPro {
 //        model.mineAssociationRules();
 //        System.out.println(model);
     
-    
-    MyFPGrowthModel model = new MyFPGrowthModel("C:\\Program Files\\Weka-3-8-5\\data\\supermarket.arff",
-                                "-P 2 -I -1 -N 10 -T 0 -C 0.8 -D 0.05 -U 1.0 -M 0.2", 
-                                "-R 1-9,11,57,70,79-81,88-89,98,100-102,107-114,116-120,122-130,137-179,189,192-199,201-216");
-    model.mineAssociationRules();
-    System.out.println(model);
+//    MyFPGrowthModel model = new MyFPGrowthModel("C:\\Program Files\\Weka-3-8-5\\data\\supermarket.arff",
+//                                "-P 2 -I -1 -N 10 -T 0 -C 0.8 -D 0.05 -U 1.0 -M 0.2", 
+//                                "-R 1-9,11,57,70,79-81,88-89,98,100-102,107-114,116-120,122-130,137-179,189,192-199,201-216");
+//    model.mineAssociationRules();
+//    System.out.println(model);
 
 //    MyFPGrowthModel model = new MyFPGrowthModel("C:\\Program Files\\Weka-3-8-5\\data\\weather.nominal.arff",
 //    "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2", 
@@ -48,5 +47,20 @@ public class WekaPro {
 //    model.mineAssociationRules();
 //    System.out.println(model);
 //    } 
+            //RemovePercentage
+//        MyKnowleadgeModel model = new MyKnowleadgeModel("C:\\Program Files\\Weka-3-8-5\\data\\iris.arff", null, null);
+//        model.trainset = model.divideTrainTest(model.dataset, 30, false);
+//        model.testset = model.divideTrainTest(model.dataset, 30, true);
+//        System.out.println(model);
+//        System.out.println(model.trainset.toSummaryString());
+//        System.out.println(model.testset.toSummaryString());
+
+            //Resample
+        MyKnowleadgeModel model = new MyKnowleadgeModel("C:\\Program Files\\Weka-3-8-5\\data\\iris.arff", null, null);
+        model.trainset = model.divideTrainTestR(model.dataset, 70, false);
+        model.testset = model.divideTrainTestR(model.dataset, 70, true);
+        System.out.println(model);
+        System.out.println(model.trainset.toSummaryString());
+        System.out.println(model.testset.toSummaryString());
     } 
 }
