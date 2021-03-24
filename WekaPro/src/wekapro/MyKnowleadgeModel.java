e5a1b8d38b57a27c7ccaa476db84cac3cb63007c
Yen Sa <ttysa98tcv@gmail.com>
2021-03-24 22:12:41 +0700

    test

--- WekaPro/src/wekapro/MyKnowleadgeModel.java
+++ WekaPro/src/wekapro/MyKnowleadgeModel.java
@@ -23,6 +23,7 @@ import weka.filters.unsupervised.instance.Resample;
  * @author ttysa
  */
 public class MyKnowleadgeModel {
+
     DataSource source;
     Instances dataset;
     String[] model_options;
@@ -32,85 +33,86 @@ public class MyKnowleadgeModel {
 
     public MyKnowleadgeModel() {
     }
-    
+
     public MyKnowleadgeModel(String filename,
-                            String m_opts,
-                            String d_opts) throws Exception {
-        this.source = new DataSource(filename);
-        this.dataset = source.getDataSet();
-        if(m_opts != null){
+            String m_opts,
+            String d_opts) throws Exception {
+        if (!filename.isEmpty()) {
+            this.source = new DataSource(filename);
+            this.dataset = source.getDataSet();
+        }
+        
+        if (m_opts != null) {
             this.model_options = weka.core.Utils.splitOptions(m_opts);
         }
-        if(d_opts != null) {
-        this.data_options = weka.core.Utils.splitOptions(d_opts);
+        if (d_opts != null) {
+            this.data_options = weka.core.Utils.splitOptions(d_opts);
         }
     }
 
-    public Instances removeData(Instances originalData)throws Exception {
+    public Instances removeData(Instances originalData) throws Exception {
         Remove remove = new Remove();
         remove.setOptions(data_options);
         remove.setInputFormat(originalData);
         return Filter.useFilter(originalData, remove);
- 
+
     }
-    
-    public Instances covertData(Instances originalData) throws Exception{
+
+    public Instances covertData(Instances originalData) throws Exception {
         NumericToNominal n2n = new NumericToNominal();
         n2n.setOptions(data_options);
         n2n.setInputFormat(originalData);
         return Filter.useFilter(originalData, n2n);
-            
+
     }
-    
-    public Instances convert2Binary(Instances originalData)throws Exception {
+
+    public Instances convert2Binary(Instances originalData) throws Exception {
         NominalToBinary n2b = new NominalToBinary();
         n2b.setOptions(data_options);
         n2b.setBinaryAttributesNominal(true);
         n2b.setInputFormat(originalData);
         return Filter.useFilter(originalData, n2b);
-        
-        
-                
+
     }
-    
-    public void saveData(String filename) throws IOException{
+
+    public void saveData(String filename) throws IOException {
         ArffSaver outData = new ArffSaver();
         outData.setInstances(this.dataset);
         outData.setFile(new File(filename));
         outData.writeBatch();
         System.out.println("Finished");
     }
-    
-    public void saveData2CSV(String filename) throws IOException{
+
+    public void saveData2CSV(String filename) throws IOException {
         CSVSaver outData = new CSVSaver();
         outData.setInstances(this.dataset);
         outData.setFile(new File(filename));
         outData.writeBatch();
         System.out.println("Converted");
-        
+
     }
 
     public Instances divideTrainTest(Instances originalSet,
-                                     double percent, boolean isTest) throws Exception {
+            double percent, boolean isTest) throws Exception {
         RemovePercentage rp = new RemovePercentage();
         rp.setPercentage(percent);
         rp.setInvertSelection(isTest);
         rp.setInputFormat(originalSet);
         return Filter.useFilter(originalSet, rp);
-        
+
     }
-    
+
     public Instances divideTrainTestR(Instances originalSet,
-                                     double percent, boolean isTest) throws Exception {
+            double percent, boolean isTest) throws Exception {
         Resample rs = new Resample();
         rs.setNoReplacement(true);
         rs.setSampleSizePercent(percent);
         rs.setInvertSelection(isTest);
         rs.setInputFormat(originalSet);
         return Filter.useFilter(originalSet, rs);
-        
+
     }
-    
+
     public void saveModel(String filename, Object model) throws Exception {
         weka.core.SerializationHelper.write(filename, model);
     }
@@ -118,7 +120,18 @@ public class MyKnowleadgeModel {
     public Object loadModel(String filename) throws Exception {
         return weka.core.SerializationHelper.read(filename);
     }
-    
+
+    //NaiveBayes
+    public void setTrainset(String filename) throws Exception {
+        DataSource trainSource = new DataSource(filename);
+        this.trainset = trainSource.getDataSet();
+    }
+
+    public void setTestset(String filename) throws Exception {
+        DataSource testsSource = new DataSource(filename);
+        this.testset = testsSource.getDataSet();
+    }
+
     @Override
     public String toString() {
         return dataset.toSummaryString();
